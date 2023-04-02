62521467f84ea689f96fec4827d548c95387c04a
Vicente Alencar Sampaio <vicentegalencar@gmail.com>
2023-04-02 16:38:36 -0300

    decorator-teste

--- stockControl/stockAPI/views.py
+++ stockControl/stockAPI/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,12 +9,18 @@ from .serializers import ItemSerializer
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+from django.http import JsonResponse
+from .decorators import verify_token  # Importe o decorator token_required
 
 
 # Create your views here.
+@verify_token
+
 class ItemList(generics.ListCreateAPIView):
 
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     authentication_classes = [SessionAuthentication]
     permission_classes = (IsAuthenticated,)
+
+
