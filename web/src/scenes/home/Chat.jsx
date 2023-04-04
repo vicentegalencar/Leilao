883f278e26f631586ebc12f372e7de23328c5e1e
Vicente Alencar Sampaio <vicentegalencar@gmail.com>
2023-04-04 00:18:04 -0300

    chat-quase-pronto

--- web/src/scenes/home/Chat.jsx
+++ web/src/scenes/home/Chat.jsx
@@ -4,9 +4,32 @@ import io from 'socket.io-client';
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
-  
+  const socketRef = useRef();
 
-  
+  useEffect(() => {
+    socketRef.current = io.connect('http://localhost:4001'); 
+
+    socketRef.current.emit('ehlo', {auction_id:1,user_id:Math.random()});
+
+    socketRef.current.on('comment', (message) => {
+      console.log(message);
+      setMessages((prevMessages) => [...prevMessages, message.message.text]);
+    });
+    console.log("oi");
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (inputMessage.trim()) {
+      socketRef.current.emit('comment', {text:inputMessage});
+      setInputMessage('');
+    }
+  };
+
 
   return (
     <div className="flex flex-col w-full  h-96 border border-gray-300 rounded bg-white">
@@ -17,10 +40,11 @@ const Chat = () => {
           </div>
         ))}
       </div>
-      <form className="flex p-2 border-t border-gray-300 bg-gray-100">
+      <form onSubmit={sendMessage} className="flex p-2 border-t border-gray-300 bg-gray-100">
         <input
           type="text"
-        //   value={inputMessage}
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
           placeholder="Digite sua mensagem..."
           className="flex-1 px-2 py-1 mr-2 border border-gray-300 rounded outline-none"
         />
