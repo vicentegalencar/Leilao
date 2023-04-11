ac641fa3faef6e2561dd75c93af1f8476fcf1ec7
Gabriel Santos <77673388+GansoHub@users.noreply.github.com>
2023-04-10 21:25:08 -0300

    actualBid

--- gerenciador/myapi/myapp/models.py
+++ gerenciador/myapi/myapp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
 class Leilao(models.Model):
     itemID = models.IntegerField()
     name = models.CharField(max_length=100, null=False, blank=False)
@@ -22,9 +27,20 @@ class Lance(models.Model):
     valor = models.DecimalField(max_digits=50, decimal_places=2, null=False, blank=False)
     horario_lance = models.DateTimeField(auto_now_add=True)
     leilao = models.ForeignKey(Leilao, on_delete=models.CASCADE)
+      
+
 
     def __str__(self):
         return self.name
+
+
+@receiver(post_save, sender=Lance)
+def my_handler(sender, instance, created, **kwargs):
+    if created:
+        instance.leilao.actualBid=instance.valor
+        instance.leilao.save()
+        
+
 
 #class CriarLance(models.Model):
 #    nome = models.CharField(max_length=100, null=False, blank=False)
